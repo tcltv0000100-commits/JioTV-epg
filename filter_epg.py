43de82db1d363c70691020115ab1f4af6543b19f
tcltv0000100-commits <tcltv0000100@gmail.com>
2026-09-20 15:17:02 +0530

    Update filter_epg.py

--- filter_epg.py
+++ filter_epg.py
@@ -7,134 +7,134 @@ OUTPUT_XML = "filtered_epg.xml"
 OUTPUT_GZ = "filtered_epg.xml.gz"
 
 # ==============================
-# ✅ CHANNEL LIST (PLAIN FORMAT)
+# ✅ CHANNEL LIST (UPDATED LOGOS - HD PREFERRED)
 # channel [space] logo_url(optional)
 # ==============================
 
 CHANNELS_TEXT = """
-9x.jalwa.in http://billu.xyz/logos2/9x_jalwa.jpg
-9x.jhakaas.in http://b1gchlogos.xyz/wp-content/uploads/2023/08/Zee-Bihar-Jharkhand.png
-9xm.in http://billu.xyz/logos2/9Xm.jpg
-9x.tashan.in
-aaj.tak.in http://b1gchlogos.xyz/wp-content/uploads/2023/08/Aaj-Tak-HD.png
-and.flix.hd.in http://billu.xyz/logos2/&flixhd.jpg
-and.pictures.hd.in http://billu.xyz/logos2/&pictureshd.jpg
-And.Prive.HD.in  https://jiotv.catchup.cdn.jio.com/dare_images/images/And_Prive_HD.png
-and.tv.hd.in http://billu.xyz/logos2/&tvhd.jpg
-and.xplorhd.in https://go4.pw/India/&explorer.png
-animal.planet.hd.world.in http://billu.xyz/logos2/animal_planethd.jpg
-b4u.kadak.in http://billu.xyz/logos2/b4u_kadak.jpg
-b4u.movies.in http://billu.xyz/logos2/b4u_movies.jpg
-b4u.music.in http://billu.xyz/logos2/b4u_music.jpg
-big.magic.in http://billu.xyz/logos2/zee_bigmagic.jpg
-cartoon.network.in http://billu.xyz/logos2/catoon_network_hd+.jpg
-cn.hd+.english.in http://billu.xyz/logos2/catoon_network_hd+.jpg
-Colors.Cineplex.HD.in http://billu.xyz/logos2/colors_cineplexhd.jpg
-colors.cineplex.superhits.in http://billu.xyz/logos2/colors_cineplex.jpg
-colors.hd.in http://billu.xyz/logos2/colorshd.jpg
-colors.infinity.hd.in http://billu.xyz/logos2/colors_infinityhd.jpg
-colors.rishtey.in http://billu.xyz/logos2/colors_rishtey.jpg
-colors.super.in http://billu.xyz/logos2/colors_rishtey.jpg
-dangal.2.in http://billu.xyz/logos2/dangal2.jpg
-dangal.in http://billu.xyz/logos2/dangal.jpg
-dd.sports.in http://billu.xyz/logos2/dd_sports.jpg
-dhoom.music.in
-discovery.hd.world.in http://billu.xyz/logos2/discovery_worldhd.jpg
-discovery.kids.in http://billu.xyz/logos2/discovery_kids.jpg
-discovery.science.in
-discovery.turbo.in
-disney.channel.in http://b1gchlogos.xyz/wp-content/uploads/2023/08/Disney.png
-disney.junior.in 
-eurosports.hd.in
-Food.Food.in
-history.tv18.hd.in http://billu.xyz/logos2/history_tv18.jpg
-hungama.in
-india.tv.in http://b1gchlogos.xyz/wp-content/uploads/2023/08/India-TV.png
-Sony.Max.HD.in http://billu.xyz/logos2/sony_maxhd.jpg
-mn+.hd.in http://billu.xyz/logos2/mn+moviedhd.jpg
-mnx.hd.in http://billu.xyz/logos2/mnx_hd.jpg
-movies.now.in
-Movies.Now.HD.in http://billu.xyz/logos2/movies_nowhd.jpg
-mtv.hd.in http://billu.xyz/logos2/mtv+hd.jpg
-mtv.in
-nat.geo.wild.hd.in http://billu.xyz/logos2/nat_geo_wildhd.jpg
-national.geographic.hd.in http://billu.xyz/logos2/net_geo_hindihd.jpg
-nick.hd+.in http://billu.xyz/logos2/nick+hd.jpg
-nick.in http://billu.xyz/logos2/nickhd.jpg
-nick.jr.in http://billu.xyz/logos2/nick_jr.jpg
-pogo.in http://billu.xyz/logos2/pogo.jpg
-romedy.now.in http://billu.xyz/logos2/romedy_nowhd.jpg
-sab.hd.in http://billu.xyz/logos2/sony_sabhd.jpg
-set.hd.in http://billu.xyz/logos2/sony_tvhd.jpg
-showbox.in http://billu.xyz/logos2/showbox.jpg
-sonic.nickelodeon.in http://billu.xyz/logos2/nick_sonic.jpg
-sony.bbc.earth.hd.in http://billu.xyz/logos2/sony_bbc_earthhd.jpg
-sony.max.1.in
-sony.max.2.in http://billu.xyz/logos2/sony_max2.jpg
-sony.pal.in http://billu.xyz/logos2/sony_pal.jpg
-sony.pix.hd.in http://billu.xyz/logos2/sony_pixhd.jpg
-sony.sab.in http://billu.xyz/logos2/sony_sabhd.jpg
-sony.sports.ten.1.hd.in http://billu.xyz/logos2/sony_ten1.jpg
-sony.sports.ten.2.hd.in http://billu.xyz/logos2/sony_ten2.jpg
-sony.sports.ten.3.hd.in http://billu.xyz/logos2/sony_ten3.jpg
-sony.sports.ten.5.hd.in http://billu.xyz/logos2/sony_ten5.jpg
-sony.wah.in http://billu.xyz/logos2/sony_wah.jpg
-sony.yay.in http://billu.xyz/logos2/sony_yay.jpg
-star.bharat.hd.in http://billu.xyz/logos2/star_bharathd.jpg
-Star.Gold.2.HD.in http://billu.xyz/logos2/star_gold2.jpg
-star.gold.in http://billu.xyz/logos2/star_gold2.jpg
-star.gold.hd.in http://billu.xyz/logos2/star_goldhd.jpg
-star.gold.romance.in
-star.gold.select.hd.in 
-star.gold.thrills.in 
-star.movies.hd.in http://billu.xyz/logos2/star_movieshd.jpg
-star.movies.select.hd.in http://billu.xyz/logos2/star_movies_selecthd.jpg
-star.plus.hd.in http://billu.xyz/logos2/star_plushd.jpg
-star.sports.1.hd.hindi.in 
-star.sports.1.hd.in http://billu.xyz/logos2/star_sports1hd.jpg
-star.sports.2.hd.in http://billu.xyz/logos2/star_sports2.jpg
-star.sports.2.hindi.hd.in http://billu.xyz/logos2/star_sports2.jpg
-star.sports.3.in http://billu.xyz/logos2/star_sports_3.jpg
-Star.Sports.First.in
-star.sports.khel.in http://billu.xyz/logos2/sports18_khel.jpg
-star.sports.select.1.hd.in http://billu.xyz/logos2/star_sports_select_1hd.jpg
-star.sports.select.2.hd.in http://billu.xyz/logos2/star_sports_select2.jpg
-star.utsav.in http://billu.xyz/logos2/star_utsav.jpg
-star.utsav.movies.in
-tlc.hd.in http://billu.xyz/logos2/tlc_hd.jpg
-Travelxp.HD.in http://b1gchlogos.xyz/wp-content/uploads/2023/08/TravelXP4K.png
-tv9.kannada.in
-tv9.marathi.in
-zee.action.in http://billu.xyz/logos2/zee_action.jpg
-zee.anmol.cinema.2.in
-zee.anmol.cinema.in http://billu.xyz/logos2/zee_anmolcinema.jpg
-zee.anmol.in http://billu.xyz/logos2/zee_anmol.jpg
-zee.bollywood.in http://billu.xyz/logos2/zee_bollywood.jpg
-zee.cinema.hd.in http://billu.xyz/logos2/zee_cinema.jpg
-zee.classic.in http://billu.xyz/logos2/zee_classic.jpg
-zee.tv.hd.in http://billu.xyz/logos2/zee_tvhd.jpg
-zing.in http://billu.xyz/logos2/zing.jpg
-zoom.in http://billu.xyz/logos2/zoom.jpg
-skysp.cricket.in
-astro.cricket.in http://billu.xyz/logos2/astro_cricket.jpg
-Astro.Cricket.my
-foxcricket.in
-willow.cricket.hd.in http://billu.xyz/logos2/willowhd.jpg
-willow.xtra.in http://billu.xyz/logos2/willowxtra.jpg
-supersport.school.hd.in http://billu.xyz/logos2/supersports_cricket.jpg
-mastiii.in http://billu.xyz/logos2/mastiii.jpg
-music.india.in http://billu.xyz/logos2/music_india.jpg
-star.sports.first.in https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBjneDJZWkUzWhrFzbHfXwnynpigT-867HEA&s
-food.food.in http://billu.xyz/logos2/food_food.jpg
-baby.tv.english.(gb,en).in http://billu.xyz/logos2/baby_tvhd.jpg
+9x.jalwa.in https://jiotv.catchup.cdn.jio.com/dare_images/images/9X_Jalwa.png
+9x.jhakaas.in https://upload.wikimedia.org/wikipedia/en/3/3d/9X_Jhakaas_logo.png
+9xm.in https://jiotv.catchup.cdn.jio.com/dare_images/images/9XM.png
+9x.tashan.in https://jiotv.catchup.cdn.jio.com/dare_images/images/9X_Tashan.png
+aaj.tak.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Aaj_Tak_HD.png
+and.flix.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/And_Flix_HD.png
+and.pictures.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/And_Pictures_HD.png
+And.Prive.HD.in https://jiotv.catchup.cdn.jio.com/dare_images/images/And_Prive_HD.png
+and.tv.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/And_TV_HD.png
+and.xplorhd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Xplor_HD.png
+animal.planet.hd.world.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Animal_Planet_HD_World.png
+b4u.kadak.in https://jiotv.catchup.cdn.jio.com/dare_images/images/B4U_Kadak.png
+b4u.movies.in https://jiotv.catchup.cdn.jio.com/dare_images/images/B4U_Movies.png
+b4u.music.in https://jiotv.catchup.cdn.jio.com/dare_images/images/B4U_Music.png
+big.magic.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Big_Magic.png
+cartoon.network.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Cartoon_Network.png
+cn.hd+.english.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Cartoon_Network_HD.png
+Colors.Cineplex.HD.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_Cineplex_HD.png
+colors.cineplex.superhits.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_Cineplex_Superhits.png
+colors.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_HD.png
+colors.infinity.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_Infinity_HD.png
+colors.rishtey.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_Rishtey.png
+colors.super.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Colors_Super.png
+dangal.2.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Dangal_2.png
+dangal.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Dangal.png
+dd.sports.in https://jiotv.catchup.cdn.jio.com/dare_images/images/DD_Sports.png
+dhoom.music.in https://upload.wikimedia.org/wikipedia/en/a/a2/Dhoom_Music_Logo.png
+discovery.hd.world.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Discovery_HD.png
+discovery.kids.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Discovery_Kids.png
+discovery.science.in https://upload.wikimedia.org/wikipedia/commons/e/e6/Discovery_Science_logo.png
+discovery.turbo.in https://upload.wikimedia.org/wikipedia/commons/1/1d/Discovery_Turbo_logo.png
+disney.channel.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Disney_Channel.png
+disney.junior.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Disney_Junior.png
+eurosports.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Eurospot_HD.png
+Food.Food.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Food_Food.png
+history.tv18.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/History_TV18_HD.png
+hungama.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Hungama_TV.png
+india.tv.in https://jiotv.catchup.cdn.jio.com/dare_images/images/India_TV.png
+Sony.Max.HD.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Max_HD.png
+mn+.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/MN_Plus_HD.png
+mnx.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/MNX_HD.png
+movies.now.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Movies_Now.png
+Movies.Now.HD.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Movies_Now_HD.png
+mtv.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/MTV_HD_Plus.png
+mtv.in https://jiotv.catchup.cdn.jio.com/dare_images/images/MTV.png
+nat.geo.wild.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Nat_Geo_Wild_HD.png
+national.geographic.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/National_Geographic_HD.png
+nick.hd+.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Nick_HD_Plus.png
+nick.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Nickelodeon.png
+nick.jr.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Nick_Jr.png
+pogo.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Pogo.png
+romedy.now.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Romedy_Now.png
+sab.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_SAB_HD.png
+set.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/SET_HD.png
+showbox.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Showbox.png
+sonic.nickelodeon.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sonic_Nickelodeon.png
+sony.bbc.earth.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_BBC_Earth_HD.png
+sony.max.1.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Max.png
+sony.max.2.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Max_2.png
+sony.pal.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Pal.png
+sony.pix.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Pix_HD.png
+sony.sab.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_SAB_HD.png
+sony.sports.ten.1.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Sports_Ten_1_HD.png
+sony.sports.ten.2.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Sports_Ten_2_HD.png
+sony.sports.ten.3.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Sports_Ten_3_HD.png
+sony.sports.ten.5.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Sports_Ten_5_HD.png
+sony.wah.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Wah.png
+sony.yay.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sony_Yay.png
+star.bharat.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Bharat_HD.png
+Star.Gold.2.HD.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Gold_2_HD.png
+star.gold.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Gold.png
+star.gold.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Gold_HD.png
+star.gold.romance.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Gold_Romance.png
+star.gold.select.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Gold_Select_HD.png
+star.gold.thrills.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Gold_Thrills.png
+star.movies.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Movies_HD.png
+star.movies.select.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Movies_Select_HD.png
+star.plus.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Plus_HD.png
+star.sports.1.hd.hindi.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_1_Hindi_HD.png
+star.sports.1.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_1_HD.png
+star.sports.2.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_2_HD.png
+star.sports.2.hindi.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_2_Hindi_HD.png
+star.sports.3.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_3.png
+Star.Sports.First.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_First.png
+star.sports.khel.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_Khel.png
+star.sports.select.1.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_Select_1_HD.png
+star.sports.select.2.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_Select_2_HD.png
+star.utsav.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Utsav.png
+star.utsav.movies.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Utsav_Movies.png
+tlc.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/TLC_HD.png
+Travelxp.HD.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Travelxp_HD.png
+tv9.kannada.in https://jiotv.catchup.cdn.jio.com/dare_images/images/TV9_Kannada.png
+tv9.marathi.in https://jiotv.catchup.cdn.jio.com/dare_images/images/TV9_Marathi.png
+zee.action.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Action.png
+zee.anmol.cinema.2.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Anmol_Cinema.png
+zee.anmol.cinema.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Anmol_Cinema.png
+zee.anmol.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Anmol.png
+zee.bollywood.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Bollywood.png
+zee.cinema.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Cinema_HD.png
+zee.classic.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Classic.png
+zee.tv.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_TV_HD.png
+zing.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zing.png
+zoom.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zoom.png
+skysp.cricket.in https://upload.wikimedia.org/wikipedia/en/3/37/Sky_Sports_Cricket_logo_2020.png
+astro.cricket.in https://upload.wikimedia.org/wikipedia/commons/d/df/Astro_Cricket_Logo.png
+Astro.Cricket.my https://upload.wikimedia.org/wikipedia/commons/d/df/Astro_Cricket_Logo.png
+foxcricket.in https://upload.wikimedia.org/wikipedia/en/3/37/Fox_Cricket_logo.png
+willow.cricket.hd.in https://upload.wikimedia.org/wikipedia/commons/c/c3/Willow_TV_logo.png
+willow.xtra.in https://upload.wikimedia.org/wikipedia/commons/c/c3/Willow_TV_logo.png
+supersport.school.hd.in https://upload.wikimedia.org/wikipedia/en/e/e0/SuperSport_logo.png
+mastiii.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Mastiii.png
+music.india.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Music_India.png
+star.sports.first.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Star_Sports_First.png
+food.food.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Food_Food.png
+baby.tv.english.(gb,en).in https://upload.wikimedia.org/wikipedia/commons/c/c5/BabyTV_logo.png
 star.life.in https://upload.wikimedia.org/wikipedia/commons/6/63/Star_Life_%28India%29.svg
-the.q.india.in http://billu.xyz/logos2/the_q.jpg
-goldmines.in http://billu.xyz/logos2/goldmines.jpg
-haryana.beats.in https://mym2e.fun/images/pJALEGYQDgwbUdRmcM-QRYikR-k2jDWt-xcbL2zgSTAqjv56J5E-3muEdNejl7UxxhhbsH87JYFl2k-RrMmzbg.jpeg
-vh1.hd.in https://mym2e.fun/images/pJALEGYQDgwbUdRmcM-QRYikR-k2jDWt-xcbL2zgSTAqjv56J5E-3muEdNejl7UxxhhbsH87JYFl2k-RrMmzbg.jpeg
-bollywood.hd.in https://s1.vcdn.biz/static/f/5512601881/image.jpg
-sports.18.2.in http://billu.xyz/logos2/sports18_khel.jpg
-Sky.Sports.Cricket.HD.ie
+the.q.india.in https://jiotv.catchup.cdn.jio.com/dare_images/images/The_Q.png
+goldmines.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Goldmines.png
+haryana.beats.in https://jiotv.catchup.cdn.jio.com/dare_images/images/MH_One_Dilse.png
+vh1.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Vh1_HD.png
+bollywood.hd.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Zee_Bollywood.png
+sports.18.2.in https://jiotv.catchup.cdn.jio.com/dare_images/images/Sports18_Khel.png
+Sky.Sports.Cricket.HD.ie https://upload.wikimedia.org/wikipedia/en/3/37/Sky_Sports_Cricket_logo_2020.png
 """
 
 # ==============================
